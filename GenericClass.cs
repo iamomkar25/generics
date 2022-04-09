1d849f2dc88bac23e2892fee503f6bae18248a1c
omkar Rakesh Shinde <omkar25shinde@gmail.com>
2022-04-09 06:55:14 +0530

    added-maximum-float

---
+++ GenericClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class GenericClass<T>
+    {
+        public T[] arr;
+        public GenericClass(T[] arr)
+        {
+            this.arr = arr;
+        }
+        public void PrintAnArray()
+        {
+            Console.WriteLine("\n Array elements");
+            foreach (var element in arr)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
---
+++ GenericPrintArray.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class GenericPrintArray
+    {
+        public static void PrintAnArray<T>(T[] arr)
+        {
+
+            Console.WriteLine("\nArray elements");
+            foreach (T element in arr)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
