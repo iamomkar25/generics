a5946ca7280f3f3dd96fadb0d2df0dc8a98b0511
omkar Rakesh Shinde <omkar25shinde@gmail.com>
2022-04-09 06:51:26 +0530

    Added-printarray

---
+++ PrintArray.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class PrintArray
+    {
+        public static void PrintAnArray(int[] arr)
+        {
+            Console.WriteLine("Integer array elements");
+            foreach (int element in arr)
+            {
+                Console.WriteLine(element);
+            }
+        }
+        public static void PrintAnArray(double[] arr)
+        {
+            Console.WriteLine("\nDouble array elements");
+            foreach (double element in arr)
+            {
+                Console.WriteLine(element);
+            }
+        }
+        public static void PrintAnArray(char[] arr)
+        {
+            Console.WriteLine("\nCharacter array elements");
+            foreach (char element in arr)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
